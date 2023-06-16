f5c3541d188d6be8cb600884bc86346e39742688
Joe Letner <jrletner@gmail.com>
2023-06-15 19:09:19 -0500

    commit models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_06_15_235812) do
   create_table "child_categories", force: :cascade do |t|
     t.string "title"
@@ -71,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_15_235812) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "child_categories", "parent_categories", column: "parent_categories_id"
@@ -79,7 +79,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_15_235812) do
   add_foreign_key "notes", "child_categories"
   add_foreign_key "notes", "users"
   add_foreign_key "parent_categories", "users", column: "users_id"
-=======
-ActiveRecord::Schema[7.0].define(version: 0) do
-
 end
