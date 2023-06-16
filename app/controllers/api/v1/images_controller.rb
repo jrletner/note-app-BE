21a74314c9e45dd34cd5e9fb3dd7717523ed3a4b
Joe Letner <jrletner@gmail.com>
2023-06-15 21:07:20 -0500

    added routes and controllers

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class UsersController < Api::V1::ApplicationController
+    class ImagesController < Api::V1::ApplicationController
       def index
         render json: { message: "this is right", status: 200 }
       end
--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,6 +1,21 @@
 module Api
   module V1
-    class NoteController < ApplicationController
+    class NotesController < Api::V1::ApplicationController
+      def index
+        render json: { message: "this is right", status: 200 }
+      end
+
+      def show
+        render json: { message: "this is right", status: 200 }
+      end
+
+      def destroy
+        render json: { message: "this is right", status: 200 }
+      end
+
+      def update
+        render json: { message: "this is right", status: 200 }
+      end
     end
   end
 end
