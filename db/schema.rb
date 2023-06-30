6b93f7fb8c5c20117afc5a0b54a2d91b0711f6a6
tguardado1994 <111324014+tguardado1994@users.noreply.github.com>
2023-06-29 19:03:11 -0500

    swagger users
    added swagger users

--- db/schema.rb
+++ db/schema.rb
@@ -49,11 +49,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_223018) do
   end
 
   create_table "parent_categories", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.integer "users_id", null: false
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_parent_categories_on_user_id"
+    t.index ["users_id"], name: "index_parent_categories_on_users_id"
   end
 
   create_table "tokens", force: :cascade do |t|
@@ -72,7 +72,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_223018) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "child_categories", "parent_categories", column: "parent_categories_id"
@@ -80,5 +79,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_223018) do
   add_foreign_key "links", "notes", column: "notes_id"
   add_foreign_key "notes", "child_categories"
   add_foreign_key "notes", "users"
-  add_foreign_key "parent_categories", "users"
+  add_foreign_key "parent_categories", "users", column: "users_id"
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,12 +1,20 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/users', type: :request do
-
+#login user
   path '/api/v1/users/login' do
 
     post('login user') do
+      description "Login a user"
+      tags "USERS"
+      parameter name: :login, in: :body, schema: {
+        type: "object",
+        properties: {
+          email: {type: "string"},
+          password: {type: "string"}
+        },
+      }
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -19,11 +27,19 @@ RSpec.describe 'api/v1/users', type: :request do
     end
   end
 
+  #create a user
   path '/api/v1/users/create' do
-
     post('create user') do
+      description "Create a user"
+      tags "USERS"
+      parameter name: :create, in: :body, schema: {
+        type: "object",
+        properties: {
+          email: { type: "string" },
+          password: { type: "string" },
+        },
+      }
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -36,11 +52,17 @@ RSpec.describe 'api/v1/users', type: :request do
     end
   end
 
+  #user show
   path '/api/v1/users/me' do
-
     get('me user') do
+      description "Get User"
+      tags "USERS"
+      security [bearer_auth: []]
+      parameter name: :id, in: :query, required: true, schema: {
+        type: "integer",
+        description: "ID of the user",
+      }
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -53,11 +75,17 @@ RSpec.describe 'api/v1/users', type: :request do
     end
   end
 
+  #logout user
   path '/api/v1/users/logout' do
-
     delete('logout user') do
+      description "logout user"
+      tags "USERS"
+      security [bearer_auth: []]
+      parameter name: :id, in: :query, required: true, schema: {
+        email:  {type: "string"},
+        password: {type: "string"}
+      }
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
