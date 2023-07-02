dc7c7a2e132b3deac6e0ac5f5ceb1c6b74f8e0db
Joe Letner <jrletner@gmail.com>
2023-07-01 21:25:30 -0500

    update note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,11 +1,9 @@
 class Note < ApplicationRecord
   belongs_to :user
-  belongs_to :child_category, class_name: "ChildCategory", foreign_key: "child_categories_id"
+  belongs_to :child_category
   has_many :images, dependent: :destroy
   has_many :links, dependent: :destroy
 
   validates :title, presence: true
   validates :description, presence: true
 end
-
-#test
