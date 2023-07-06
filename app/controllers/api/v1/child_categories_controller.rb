09e1e0ecf4e87d5c868b1d0485a0e63ce7c27a36
Joe Letner <jrletner@gmail.com>
2023-07-06 17:38:32 -0500

    updated formatting

--- app/controllers/api/v1/child_categories_controller.rb
+++ app/controllers/api/v1/child_categories_controller.rb
@@ -1,37 +1,35 @@
 module Api
-    module V1
-        class ChildCategoriesController < Api::V1::ApplicationController
-            def index
-                child_categories = ChildCategory.all
-                payload = {
-                    child_categories: ChildCategoryBlueprint.render_as_hash(child_categories)
-                }
-                render_success(payload, status: 200)
-            end
-        
-            def show
-                child_category = ChildCategory.find(params[:id])
-                payload = {
-                    child_category: ChildCategoryBlueprint.render_as_hash(child_category)
-                }
-                render_success(payload, status: 200)
-            end
-        
-            def destroy
-            end
-        
-            def update
-            end
-        
-            def create
-                result = Api::Child_Categories.new_child_category(params)
-                render_error(errors: "Invalid Child Category", status: 400) and return unless result.success?
-                payload = {
-                    child_category: ChildCategoryBlueprint.render_as_hash(child_category)
-                }
+  module V1
+    class ChildCategoriesController < Api::V1::ApplicationController
+      def index
+        child_categories = ChildCategory.all
+        payload = {
+          child_categories: ChildCategoryBlueprint.render_as_hash(child_categories),
+        }
+        render_success(payload, status: 200)
+      end
 
-            end
-        end
+      def show
+        child_category = ChildCategory.find(params[:id])
+        payload = {
+          child_category: ChildCategoryBlueprint.render_as_hash(child_category),
+        }
+        render_success(payload, status: 200)
+      end
+
+      def destroy
+      end
+
+      def update
+      end
+
+      def create
+        result = Api::Child_Categories.new_child_category(params)
+        render_error(errors: "Invalid Child Category", status: 400) and return unless result.success?
+        payload = {
+          child_category: ChildCategoryBlueprint.render_as_hash(child_category),
+        }
+      end
     end
+  end
 end
-
--- app/controllers/api/v1/parent_categories_controller.rb
+++ app/controllers/api/v1/parent_categories_controller.rb
@@ -1,36 +1,35 @@
 module Api
-    module V1
-        class ParentCategoriesController < Api::V1::ApplicationController
-            def index
-                parent_categories = ParentCategory.all
-                payload = {
-                    parent_categories: ParentCategoryBlueprint.render_as_hash(parent_categories)
-                }
-                render_success(payload, status: 200)
-            end
-        
-            def show
-                parent_category = ParentCategory.find(params[:id])
-                payload = {
-                    parent_category: ParentCategoryBlueprint.render_as_hash(parent_category)
-                }
-                render_success(payload, status: 200)
-            end
-        
-            def destroy
-            end
-        
-            def update
-            end
-        
-            def create
-                result = Api::Parent_Categories.new_parent_category(params)
-                render_error(errors: "Invalid Parent Category", status: 400) and return unless result.success?
-                payload = {
-                    parent_category: ParentCategoryBlueprint.render_as_hash(parent_category)
-                }
-            end
-        end
-    end
-end
+  module V1
+    class ParentCategoriesController < Api::V1::ApplicationController
+      def index
+        parent_categories = ParentCategory.all
+        payload = {
+          parent_categories: ParentCategoryBlueprint.render_as_hash(parent_categories),
+        }
+        render_success(payload, status: 200)
+      end
 
+      def show
+        parent_category = ParentCategory.find(params[:id])
+        payload = {
+          parent_category: ParentCategoryBlueprint.render_as_hash(parent_category),
+        }
+        render_success(payload, status: 200)
+      end
+
+      def destroy
+      end
+
+      def update
+      end
+
+      def create
+        result = Api::Parent_Categories.new_parent_category(params)
+        render_error(errors: "Invalid Parent Category", status: 400) and return unless result.success?
+        payload = {
+          parent_category: ParentCategoryBlueprint.render_as_hash(parent_category),
+        }
+      end
+    end
+  end
+end
