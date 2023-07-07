7f785c9a8da861fa26eb0b3053b193a46eb2377b
EAS0314 <111325608+EAS0314@users.noreply.github.com>
2023-07-06 19:04:15 -0500

    Eric-seed-tables
    Worked on created the seed tables in the schema file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+(0..10). each do
+    User.create(email: Faker::Internet.email, password_digest:"password")
+end
+
+
+# (0..10). each do 
+#     ParentCategory.create(users_id: rand(0..10), title: Faker::Hacker.noun.capitalize)
+# end
+
+# (0..10). each do 
+#     ChildCategory.create(parent_categories_id: rand(0..10), title: Faker::Hacker.noun.capitalize)
+# end
+
+Notes.create(user_id rand(1..10), child_category_id: rand(1..10), title: Faker::Hacker.noun.capitalize, 
+description: Faker::Hacker.noun.capitalize)
+
+Images.create(notes_id:rand(1..10), link_name: Faker::Hacker.noun.capitalize, image_path: Faker::Hacker.noun.capitalize)
+
+Links.create(notes_id:rand(1..10),link_name: Faker::Hacker.noun.capitalize, link: Faker::Hacker.noun.capitalize)
+
+ChildCategory.create(notes_id: rand(1..10), title: Faker::Hacker.noun.capitalize)
+
+ParentCategory.create(user_id: rand(1..10), title: Faker::Hacker.noun.capitalize)
+
