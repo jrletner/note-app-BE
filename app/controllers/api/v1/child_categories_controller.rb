6f9832fb2043e921bcd9e93eae00b8a1781e2580
Joe Letner <jrletner@gmail.com>
2023-07-13 17:26:22 -0500

    corrected child category model, controller

--- app/controllers/api/v1/child_categories_controller.rb
+++ app/controllers/api/v1/child_categories_controller.rb
@@ -12,33 +12,33 @@ module Api
       def show
         child_category = ChildCategory.find(params[:id])
         payload = {
-          child_category: ChildCategoryBlueprint.render_as_hash(child_category),
+          child_category: ChildCategoriesBlueprint.render_as_hash(child_category),
         }
-        render_success(payload, status: 200)
+        render_success(payload: payload, status: 200)
       end
 
       def destroy
-        result = Api::Child_Categories.delete_child(params)
+        result = Api::ChildCategories.delete_child(params)
         render_error(errors: "There was an error deleting the child catagory", status: 400) and return unless result.success?
         payload = {
           status: "success",
           message: "The child catagory was successfully deleted",
-          note: Child_Categories.render_as_hash(result.payload),
+          note: ChildCategoriesBlueprint.render_as_hash(result.payload),
           status: 201,
         }
         render_success(payload: payload)
       end
 
       def update
-        prev_child = Child_Categories.find(params[:id])
-        result = Api::Child_Categories.update_parent(params)
+        prev_child = ChildCategory.find(params[:id])
+        result = Api::ChildCategories.update_child(params)
         render_error(errors: "There was an error updating the child catagory", status: 400) and return unless result.success?
         previous_values = {
           id: prev_child[:id],
           title: prev_child[:title],
         }
         payload = {
-          note: Child_Categories.render_as_hash(result.payload),
+          note: ChildCategoriesBlueprint.render_as_hash(result.payload),
           previous_values: previous_values,
           status: 201,
         }
@@ -46,11 +46,12 @@ module Api
       end
 
       def create
-        result = Api::Child_Categories.new_child_category(params)
+        result = Api::ChildCategories.new_child_category(params)
         render_error(errors: "Invalid Child Category", status: 400) and return unless result.success?
         payload = {
-          child_category: ChildCategoryBlueprint.render_as_hash(child_category),
+          child_category: ChildCategoriesBlueprint.render_as_hash(result.payload),
         }
+        render_success(payload: payload, status: 201)
       end
     end
   end
--- app/services/api/child_categories.rb
+++ app/services/api/child_categories.rb
@@ -1,39 +1,38 @@
 module Api
-    module Child_Categories
-        def self.new_child_category(params)
-            child_category = ChildCategory.new(title: params[:title])
+  module ChildCategories
+    def self.new_child_category(params)
+      child_category = ChildCategory.new(title: params[:title], parent_category_id: params[:parent_category_id])
 
-            begin
-                child_category.save!
-            rescue
-                return ServiceContract.error("Error") unless child_category.vaild?
-            end
+      begin
+        child_category.save!
+      rescue
+        return ServiceContract.error("Error") unless child_category.valid?
+      end
 
-            ServiceContract.success(child_category)
-        end
-
-        def self.delete_child(params)
-            child = Child_Categories.find_by(id: params[:id])
-      
-            return ServiceContract.error("Child catagory not found") unless child
-      
-            if child.destroy
-              ServiceContract.success(child)
-            else
-              ServiceContract.error("Failed to delete child category")
-            end
-        end
-        def self.update_child(params)
-            child = Child_Categories.find_by(id: params[:id])
-      
-            return ServiceContract.error("Child Catagory not found") unless child
-      
-            if child.update(title: params[:title])
-              ServiceContract.success(child)
-            else
-              ServiceContract.error("Failed to update child category")
-            end
-          end
-
+      ServiceContract.success(child_category)
     end
+
+    def self.delete_child(params)
+      child = ChildCategory.find_by(id: params[:id])
+
+      return ServiceContract.error("Child catagory not found") unless child
+
+      if child.destroy
+        ServiceContract.success(child)
+      else
+        ServiceContract.error("Failed to delete child category")
+      end
+    end
+    def self.update_child(params)
+      child = ChildCategory.find_by(id: params[:id])
+
+      return ServiceContract.error("Child Catagory not found") unless child
+
+      if child.update(title: params[:title])
+        ServiceContract.success(child)
+      else
+        ServiceContract.error("Failed to update child category")
+      end
+    end
+  end
 end
