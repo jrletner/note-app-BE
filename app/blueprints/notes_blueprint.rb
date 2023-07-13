1a98a243074b6a8376ecbe5dc5f83436f4cd3215
tguardado1994 <111324014+tguardado1994@users.noreply.github.com>
2023-07-13 18:15:10 -0500

    Update notes_blueprint.rb

--- app/blueprints/notes_blueprint.rb
+++ app/blueprints/notes_blueprint.rb
@@ -2,4 +2,5 @@ class NotesBlueprint < Blueprinter::Base
   identifier :id
   fields :title, :description
   association :user, blueprint: UserBlueprint
+  association :child_category, blueprint: ChildCategoryBlueprint
 end
